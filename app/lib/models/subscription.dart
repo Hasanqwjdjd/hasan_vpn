344c9a040d7563ef5d761d23f2893b09235bbb91
Hasanqwjdjd <mohamadbeiky5@gmail.com>
2026-09-19 02:30:21 +0330

    Update subscription.dart

--- app/lib/models/subscription.dart
+++ app/lib/models/subscription.dart
@@ -2,6 +2,7 @@ class Subscription {
   final String id;
   final String name;
   final String url;
+  final bool isDefault;
   bool autoUpdate;
   DateTime? lastUpdated;
   int serverCount;
@@ -10,6 +11,7 @@ class Subscription {
     required this.id,
     required this.name,
     required this.url,
+    this.isDefault = false,
     this.autoUpdate = true,
     this.lastUpdated,
     this.serverCount = 0,
@@ -19,6 +21,7 @@ class Subscription {
         'id': id,
         'name': name,
         'url': url,
+        'isDefault': isDefault,
         'autoUpdate': autoUpdate,
         'lastUpdated': lastUpdated?.toIso8601String(),
         'serverCount': serverCount,
@@ -28,6 +31,7 @@ class Subscription {
         id: json['id'] as String,
         name: json['name'] as String,
         url: json['url'] as String,
+        isDefault: json['isDefault'] as bool? ?? false,
         autoUpdate: json['autoUpdate'] as bool? ?? true,
         lastUpdated: json['lastUpdated'] != null
             ? DateTime.tryParse(json['lastUpdated'] as String)
