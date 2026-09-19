c57821ed66d413c2c56a44658bc6a0432ec4643d
Hasanqwjdjd <mohamadbeiky5@gmail.com>
2026-09-19 13:38:24 +0330

    Update settings_service.dart

--- app/lib/services/settings_service.dart
+++ app/lib/services/settings_service.dart
@@ -1,9 +1,17 @@
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class SettingsService {
-  static const String currentVersion = '6.0.0';
+  static String currentVersion = '9.0.94';
   static const String _themeKey = 'settings_theme_mode_v2';
   static const String _langKey = 'settings_language_v2';
+
+  static Future<void> loadVersion() async {
+    try {
+      final info = await PackageInfo.fromPlatform();
+      currentVersion = info.version;
+    } catch (_) {}
+  }
 
   static Future<String> getThemeMode() async {
     final prefs = await SharedPreferences.getInstance();
